c79774363a69c2e6381d2b7eb91d00739125fea8
Mark <mark_xiao@xreply.cn>
2019-08-16 08:15:38 +0800

    task memory update.

---
+++ virtual_mem_test/task_line_memory.c
@@ -0,0 +1,15 @@
+#include "task_line_memory.h"
+#include "virtual_mem.h"
+
+U64 task_malloc(U32 size) {
+    return 0;
+ }
+U32 task_write(U64 pos, U8 data ) {
+    return 0;
+}
+U8 task_read(U64 pos) {
+    return 0;
+}
+U32 task_freem(U64 pos) {
+    return 0;
+}
